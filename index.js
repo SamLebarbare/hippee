82b4e4626db0470ca2970ad0e092ddf8f72392ae
Samuel Loup <samuel.loup@gmail.com>
2019-02-09 22:23:48 +0100

    Parse ip

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const lineReader = require("readline").createInterface({
   input: require("fs").createReadStream("./ip2loc.csv")
 });
 
+const long2ip = ip =>
+  [ip >>> 24, (ip >>> 16) & 0xff, (ip >>> 8) & 0xff, ip & 0xff].join(".");
+
 const extract = watt(function*(next) {
   const res = yield _("line", lineReader)
     .map(line => {
@@ -15,8 +18,8 @@ const extract = watt(function*(next) {
     .take(100)
     .map(row => {
       return {
-        from: row[0],
-        to: row[1]
+        from: long2ip(row[0].replace(/"/g, "")),
+        to: long2ip(row[1].replace(/"/g, ""))
       };
     })
     .collect()
